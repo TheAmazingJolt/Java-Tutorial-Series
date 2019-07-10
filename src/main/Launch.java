91af4f6f317cb4bf9576a6bbcf18129f1f5b47b6
TheAmazingJolt <nealbraddy2018@gmail.com>
2019-07-09 20:22:41 -0400

    Inital Commit

--- src/main/Launch.java
+++ src/main/Launch.java
@@ -1,34 +1,12 @@
 package main;
 
-import java.util.Scanner;
-
 public class Launch {
 	
-	public static float a;
-	public static float b;
-	
-	public static String operation;
-	
-	public static Scanner scanner = new Scanner(System.in);
-	
-	public static Maths maths;
+	public static int num = 0;
 	
 	public static void main(String[] args) {
-		System.out.println("Type in first number: ");
-		a = scanner.nextFloat();
-		System.out.println("Type in second number: ");
-		b = scanner.nextFloat();
-		System.out.println("Type operation +, -, *, /: ");
-		operation = scanner.next();
-		maths = new Maths(a,b);
-		if(operation.contains("+")) {
-			System.out.println("Your answer is: " + maths.add());
-		}else if(operation.contains("-")){
-			System.out.println("Your answer is: " + maths.subtract());
-		}else if(operation.contains("*")) {
-			System.out.println("Your answer is: " + maths.multiply());
-		}else if(operation.contains("/")){
-			System.out.println("Your answer is: " + maths.divide());
+		for(int i = 0; i <= 5; i++) {
+			System.out.println(i);
 		}
 	}
 	
