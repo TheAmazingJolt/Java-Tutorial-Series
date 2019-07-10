5eb16f3204f3b8d8fa7a624ea68b470e1111cc99
TheAmazingJolt <nealbraddy2018@gmail.com>
2019-07-09 20:09:16 -0400

    Inital Commit

--- src/main/Launch.java
+++ src/main/Launch.java
@@ -7,6 +7,8 @@ public class Launch {
 	public static float a;
 	public static float b;
 	
+	public static String operation;
+	
 	public static Scanner scanner = new Scanner(System.in);
 	
 	public static Maths maths;
@@ -16,8 +18,18 @@ public class Launch {
 		a = scanner.nextFloat();
 		System.out.println("Type in second number: ");
 		b = scanner.nextFloat();
+		System.out.println("Type operation +, -, *, /: ");
+		operation = scanner.next();
 		maths = new Maths(a,b);
-		System.out.println("Your answer is: " + maths.multiply());
+		if(operation.contains("+")) {
+			System.out.println("Your answer is: " + maths.add());
+		}else if(operation.contains("-")){
+			System.out.println("Your answer is: " + maths.subtract());
+		}else if(operation.contains("*")) {
+			System.out.println("Your answer is: " + maths.multiply());
+		}else if(operation.contains("/")){
+			System.out.println("Your answer is: " + maths.divide());
+		}
 	}
 	
 }
